c77b3a06a6b65cafb0fcc8272b950ca7378bcb2c
Matthias Broecheler <mbroecheler@users.noreply.github.com>
2024-04-15 20:26:22 -0700

    Minor update to readme. Delete remove_source.py.

--- clickstream-recommendation/yourdata-files/remove_source.py
+++
@@ -1,9 +0,0 @@
-import gzip
-import json
-
-with gzip.open('click_part0001.json.gz', 'rt') as f:
-    for line in f:
-        data = json.loads(line)
-        if '_source_time' in data:
-            del data['_source_time']
-        print(json.dumps(data))
