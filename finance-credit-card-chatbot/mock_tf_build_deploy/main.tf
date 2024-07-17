06a4f4c5b59ee80778900c98564879c759b12f0a
Tianchen Wu <wu.tianchen.business@gmail.com>
2024-07-17 10:52:28 +0000

    rm data bucket

--- finance-credit-card-chatbot/mock_tf_build_deploy/main.tf
+++ finance-credit-card-chatbot/mock_tf_build_deploy/main.tf
@@ -2,15 +2,6 @@ locals {
   namespace  = "datasqrl"
   aws_region = "us-east-1"
 
-
-
-  local_data_locations = toset([
-    "creditcard-local/cardAssignment.jsonl",
-    "creditcard-local/merchant.jsonl",
-    "creditcard-local/merchantReward.jsonl",
-    "creditcard-local/transaction.jsonl",
-  ])
-
   engine_server_environment_variables = {
     "PROPERTIES_BOOTSTRAP_SERVERS" : "kafka:9092"
   }
@@ -29,7 +20,7 @@ locals {
 
 
 module "sqrl-aws-managed-flink-example" {
-  source = "git@github.com:DataSQRL/sqrl-cloud.git//modules/component-aws-managed-full-stack-local?ref=v0.1.0"
+  source = "git@github.com:DataSQRL/sqrl-cloud.git//modules/component-aws-managed-full-stack-local"
 
   engine_server_image = "286928876767.dkr.ecr.us-east-1.amazonaws.com/datasqrl-examples/finance-credit-card-chatbot/package-analytics-no-chat/deploy-server:latest"
 
@@ -39,6 +30,5 @@ module "sqrl-aws-managed-flink-example" {
 
   engine_flink_jar_file_path               = "FlinkJob.jar"
   engine_flink_aws_managed_property_groups = local.engine_flink_aws_managed_property_groups
-  local_data_locations                     = local.local_data_locations
   engine_server_environment_variables      = local.engine_server_environment_variables
 }
