2c46f55f8eaf3495454227b6345ab9cf33c6ce5f
Tianchen Wu <wu.tianchen.business@gmail.com>
2024-07-19 13:20:05 +0000

    test aurora

--- finance-credit-card-chatbot/mock_tf_build_deploy/main.tf
+++ finance-credit-card-chatbot/mock_tf_build_deploy/main.tf
@@ -39,7 +39,7 @@ module "sqrl-aws-managed-flink-example" {
 
 module "cluster" {
   source  = "terraform-aws-modules/rds-aurora/aws"
-  version = "8.1.0"
+  version = "9.3.0"
 
   name           = "test-aurora-db-postgres96"
   engine         = "aurora-postgresql"
