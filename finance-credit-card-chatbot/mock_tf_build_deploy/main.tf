fd0f900f51a062d0b43e02e8fedd35c143a3bdd4
Tianchen Wu <wu.tianchen.business@gmail.com>
2024-07-24 15:04:54 +0000

    switch to cdktf module

--- finance-credit-card-chatbot/mock_tf_build_deploy/main.tf
+++ finance-credit-card-chatbot/mock_tf_build_deploy/main.tf
@@ -20,7 +20,7 @@ locals {
 
 
 module "sqrl-aws-managed-flink-example" {
-  source = "git@github.com:DataSQRL/sqrl-cloud.git//modules/component-aws-managed-full-stack-local"
+  source = "git@github.com:DataSQRL/sqrl-cloud.git//cdktf.out/stacks/test-tfcdk?ref=33-refactor-top-level-maintf-with-cdktf"
 
   engine_server_image = "286928876767.dkr.ecr.us-east-1.amazonaws.com/datasqrl-examples/finance-credit-card-chatbot/package-analytics-no-chat/deploy-server:latest"
 
