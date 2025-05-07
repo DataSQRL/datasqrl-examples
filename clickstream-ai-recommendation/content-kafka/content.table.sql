ebf60e6cba3bb1dbbbf4c0e8a9856cbe68a86065
Marvin <velo@users.noreply.github.com>
2025-05-07 15:15:58 -0300

    Enable compile -c package-rewards-local.json (#49)
    * Enable compile -c package-rewards-local.json
    
    Signed-off-by: Marvin Froeder <marvin@datasqrl.com>
    
    * Also compile kafka rewards
    
    Signed-off-by: Marvin Froeder <marvin@datasqrl.com>
    
    * Fix GitHub Action to run all test commands in matrix
    
    Previously, only the first line of `test_commands` was executed due to improper handling of multiline strings. Switched to `printf` with process substitution to ensure all test commands are run sequentially.
    
    Signed-off-by: Marvin Froeder <marvin@datasqrl.com>
    
    * Fix clickstream-ai-recommendation compilation
    
    Signed-off-by: Marvin Froeder <marvin@datasqrl.com>
    
    * Disable study_analytics_snowflake_package.json
    
    Signed-off-by: Marvin Froeder <marvin@datasqrl.com>
    
    * Use path as docker tag
    
    Signed-off-by: Marvin Froeder <marvin@datasqrl.com>
    
    * Disable study_analytics_snowflake_package.json
    
    Signed-off-by: Marvin Froeder <marvin@datasqrl.com>
    
    ---------
    
    Signed-off-by: Marvin Froeder <marvin@datasqrl.com>

--- clickstream-ai-recommendation/content-kafka/content.table.sql
+++ clickstream-ai-recommendation/content-kafka/content.table.sql
@@ -2,8 +2,8 @@ CREATE TABLE Content (
      url STRING NOT NULL,
      title STRING NOT NULL,
      text STRING NOT NULL,
-     update_time TIMESTAMP_LTZ(3) NOT NULL METADATA FROM 'timestamp'
-    WATERMARK FOR update_time AS update_time - INTERVAL '0.001' SECOND
+     update_time TIMESTAMP_LTZ(3) NOT NULL METADATA FROM 'timestamp',
+     WATERMARK FOR update_time AS update_time - INTERVAL '0.001' SECOND
 ) WITH (
     'connector' = 'kafka',
     'topic' = 'content',
@@ -11,6 +11,5 @@ CREATE TABLE Content (
     'properties.group.id' = 'clickstream-group',
     'scan.startup.mode' = 'group-offsets',
     'properties.auto.offset.reset' = 'earliest',
-    'format' = 'flexible-json',
-
+    'format' = 'flexible-json'
     );
