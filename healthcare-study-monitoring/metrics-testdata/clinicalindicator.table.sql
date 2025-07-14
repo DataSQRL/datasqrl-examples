b1fe6931b71892da1fa531ab879f546c35439c0b
Ferenc Csaky <ferenc@datasqrl.com>
2025-07-14 16:13:42 +0200

    Remove the old structure of the changed examples

--- healthcare-study-monitoring/metrics-testdata/clinicalindicator.table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE ClinicalIndicator (
-     `timestamp` AS TO_TIMESTAMP_LTZ(`time`,3),
-     WATERMARK FOR `timestamp` AS `timestamp` - INTERVAL '0.001' SECOND
-) WITH (
-      'format' = 'flexible-json',
-      'path' = '${DATA_PATH}/clinicalindicator.jsonl',
-      'source.monitor-interval' = '10 min',
-      'connector' = 'filesystem'
-      );
